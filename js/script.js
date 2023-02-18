12e63a04ebce64a710f0a5bc927944b8f9434f3d
mhaemnn <ismuhaimin1@gmail.com>
2023-02-18 10:25:41 +0700

    menambahkan syntax

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-function myFunction(){
-  let x = document.getElementById("myNav")
-  if(x.className === "navbar"){
-    x.className += "responsive";
-  }else{
-    x.className = "myNav";
-  }
-}
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
+  });
+
+  burger.classList.toggle("toggle");
+});
