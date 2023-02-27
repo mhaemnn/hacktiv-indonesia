47775c8439f62fde64d836c2ea08c3a9737c80fb
mhaemnn <ismuhaimin1@gmail.com>
2023-02-28 00:04:04 +0700

    modifikasi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// header scticky
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
